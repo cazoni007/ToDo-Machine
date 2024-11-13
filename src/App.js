ff8b1d589e8470e0175d47d027b6536a471c3ccd
cazoni007 <sebastiancazon301@gmail.com>
2024-11-13 12:56:13 -0400

    finishing the functionality of the TodoCounter and TodoSearch components

--- src/App.js
+++ src/App.js
@@ -11,29 +11,40 @@ import React from 'react';
 const defaultTodos = [
     {text: 'Cortar cebolla skjhdkfj ahsdlf kjahsdkf hagsk djfgaskj dfga skdjfhagsdfj',
     completed: true },
-    {text: 'Jugar con la llorona',
+    {text: 'Jugár con la llorona',
     completed: false },
     {text: 'Acabar el curso de platzi',
     completed: false },
     {text: 'Acabar con este array xd',
-      completed: true }
+      completed: true },
+    {text: 'Acabar el curso de platzi1',
+    completed: false },
+    {text: 'Escribiendo un texto medianamente largo para ver como se comporta la pagina y si no ocurre un overflow, ya que seguramente habran personas que quieran escribir pasos o tareas largas y en ves de usar distintas lo querran poner en uno solo por lo que esto deberia funcionar normal y no bugearse, ademas poder escribir palabras como otorrinolaringologia o medescorazonare xdddddd',
+      completed: true },
+    {text: 'Nuevo tarea pro',
+    completed: false },
   ];
 
 function App() {
-  // const lista = document.getElementsByTagName('ul');
-  // const numbers = lista[0].children.length;
-  // console.log(numbers);
+  const [todos, setTodos] = React.useState(defaultTodos);
   const [searchValue, setSearchValue] = React.useState('');
+  const searchedValues = todos.filter(todo =>
+    {
+      const noTildes = (texto) => texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+      return noTildes(todo.text.toLocaleLowerCase()).includes(noTildes(searchValue.toLocaleLowerCase()));
+    }) 
   console.log(searchValue);
+  const completedTodos = todos.filter(todo => todo.completed).length;
   return (
     <MainSection>
       <ToggleTheme/>
-      <TodoCounter cantidad={4} total={5}/>
+      <TodoCounter cantidad={completedTodos} total={todos.length} setTodos={setTodos}/>
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue}/>
       <TodoList>
-        {defaultTodos.map(todo => (
-          <TodoItem key={todo.text} contenido={todo.text} completed={todo.completed}/>
-        ))} 
+        {
+           searchedValues.map(todo => (
+             <TodoItem key={todo.text} contenido={todo.text} completed={todo.completed}/>))
+        } 
       </TodoList>
       <TodoButton/>
     </MainSection>

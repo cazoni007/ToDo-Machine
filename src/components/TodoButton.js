be0fa61b84a70d098ec0f02a9a2cf41e0e23726e
cazoni007 <sebastiancazon301@gmail.com>
2024-11-21 11:15:14 -0400

    adding an arrow icon at the botton next to the add todo button while there are not any todo. Also add media queries to increment the font size when the screen is up to 1000px

--- src/components/TodoButton.js
+++ src/components/TodoButton.js
@@ -6,8 +6,11 @@ function TodoButton (props) {
     const setTheme = props.theme === "lightTheme" ? "button--lightTheme" : "button--darkTheme";
     const setAnimation = props.todoLength <= 0 ? "animation" : "button";
     const setPlusAnimation = props.todoLength <= 0 ? "plusAnimation" : "buttonIcon";  
+    const setArrowLight = props.todoLength <= 0 && props.theme === "lightTheme" ? "arrowLightAnimation" : "";
+    const setArrowDark = props.todoLength <= 0 && props.theme === "darkTheme" ? "arrowDarkAnimation" : "";
     return (
             <div className='buttonContainer'>
+                <span className={`${setArrowLight} ${setArrowDark}`}></span>
                 <button className={`${setTheme} ${setAnimation}`} onClick={props.buttonClick}>
                     <span className={`${setPlusAnimation}`}></span>
                 </button>
